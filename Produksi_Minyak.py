24e360e2609a69bd7f4bcbb744b44488b91f3a8e
YaumilSultan <96162901+YaumilSultan@users.noreply.github.com>
2021-12-16 14:08:43 +0700

    Add files via upload

---
+++ Produksi_Minyak.py
@@ -0,0 +1,130 @@
+from altair.vegalite.v4.api import Chart
+from altair.vegalite.v4.schema.channels import Color
+import streamlit as st
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+import altair as alt
+from PIL import Image
+from streamlit.elements import number_input
+
+st.title('Data Produksi Minyak Mentah dari Berbagai Negara di Seluruh Dunia')
+image = Image.open('C:\Semester 3\Iroha.png')
+st.sidebar.image(image)
+st.sidebar.title('いらっしゃいませ (Welcome)')
+st.sidebar.subheader('''Creator: M. Yaumil Sultan R.
+    NIM: 12219011''')
+
+#nomor 1 -------------------------------------------------------------------------------------------------------------------------
+
+f = open('C:\Semester 3\Komputer\kode_negara_lengkap.json')
+fjson = json.load(f)
+
+df = pd.read_csv('C:\Semester 3\Komputer\produksi_minyak_mentah.csv')
+
+df1=df['kode_negara'].values.tolist()
+df2=[]
+
+for i in df1:
+    for j in fjson:
+        if i == j['alpha-3']:
+            i = j['name']
+    df2.append(i)
+
+df['kode_negara'] = df2
+
+df.sort_values(by=['kode_negara'], ascending=True)
+df_new = df.drop_duplicates('kode_negara')
+
+option = st.selectbox(
+     'Pilih Negara:',
+     (df_new['kode_negara']))
+
+grouped = df.groupby(df['kode_negara'])
+df_new = grouped.get_group(option)
+        
+chart = alt.Chart(df_new).mark_line().encode(alt.X('tahun'),alt.Y('produksi'))
+st.altair_chart(chart, use_container_width=True)
+
+#nomor 2 ------------------------------------------------------------------------------------------------------------------------------
+
+st.subheader('Grafik Jumlah Produksi Terbesar pada Tahun Tertentu')
+
+df.sort_values(by=['tahun'], ascending=True)
+df_new = df.drop_duplicates('tahun')
+
+option1 = st.selectbox(
+     'Pilih Tahun:',
+     (df_new['tahun']))
+
+grouped = df.groupby(df['tahun'])
+df_new = grouped.get_group(option1).sort_values(by=['produksi'], ascending=False)
+
+
+number = int(st.number_input('Jumlah Negara',min_value=1, max_value=196))
+dfnew=df_new.head(number)
+
+bar1 = alt.Chart(dfnew).mark_bar().encode(alt.X('kode_negara'),alt.Y('produksi'))
+st.altair_chart(bar1, use_container_width=True)
+
+#nomor 3 ------------------------------------------------------------------------------------------------------------------------------
+
+st.subheader('Grafik Jumlah Produksi Terbesar secara Kumulatif')
+
+number1 = int(st.number_input('Negara:',min_value=1, max_value=196))
+
+negara = []
+for i in df['kode_negara']:
+    if i not in negara:
+        negara.append(i)
+
+lst = []
+for i in negara:
+    lst.append(df[df['kode_negara'] == i]['produksi'].sum())
+
+df_kumulatif = pd.DataFrame(list(zip(negara, lst)), columns =['Negara', 'Kumulatif']).sort_values(by=['Kumulatif'], ascending=False)
+
+bar2 = alt.Chart(df_kumulatif.head(number1)).mark_bar().encode(alt.X('Negara'),alt.Y('Kumulatif'))
+st.altair_chart(bar2, use_container_width=True)
+#nomor 4 ------------------------------------------------------------------------------------------------------------------------------
+
+st.subheader('Jumlah Produksi Terbesar & Terkecil Keseluruhan Tahun')
+st.write('Jumlah Produksi Terbesar Keseluruhan Tahun')
+
+st.caption("Negara: {}  \nProduksi: {}".format(
+        df_kumulatif.iloc[0]['Negara'], df_kumulatif.iloc[0]['Kumulatif']))
+
+st.write('Jumlah Produksi Terkecil Keseluruhan Tahun')
+
+reversed_df_kumulatif = df_kumulatif.iloc[::-1]
+
+lowest = []
+low = []
+for i in reversed_df_kumulatif['Kumulatif']:
+    for j in reversed_df_kumulatif['Negara']:
+        if i != 0:
+            lowest.append(i)
+            low.append(j)
+
+reversed_df_kumulatif = pd.DataFrame(list(zip(low, lowest)), columns =['Negara', 'Kumulatif'])
+
+st.caption("Negara: {}  \nProduksi: {}".format(
+        reversed_df_kumulatif.iloc[0]['Negara'], reversed_df_kumulatif.iloc[0]['Kumulatif']))
+
+
+st.subheader('Jumlah Produksi Terbesar & Terkecil Pada Tahun Tertentu')
+
+df = df.sort_values(by=['produksi'],ascending=False)
+
+df.sort_values(by=['tahun'], ascending=True)
+df_new = df.drop_duplicates('tahun')
+
+option2 = st.selectbox(
+     'Tahun:',
+     (df_new['tahun']))
+
+grouped = df.groupby(df['tahun'])
+df_option1 = grouped.get_group(option2)
+
+st.write('Jumlah Produksi Terbesar')
+st.write('Jumlah Produksi Terkecil')
